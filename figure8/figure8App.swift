8b00ed625eeb9f02c29cb0344076e7c445ee006c
Nathan Stankevicius <nathan_85@me.com>
2021-06-09 16:00:51 +1000

    Initial Commit

---
+++ figure8/figure8App.swift
@@ -0,0 +1,17 @@
+//
+//  figure8App.swift
+//  figure8
+//
+//  Created by Nathan Stankevicius on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct figure8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
